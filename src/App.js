9e844e8f6cc068ee7e09b66211da619c6ca57fdb
shabaz ali malik <shabazalimalik69@gmail.com>
2022-09-23 10:01:55 +0530

    Github Stat Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
+import GitHubStat from "./components/GitHubStat";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -15,6 +16,7 @@ function App() {
        <TechStack/>
        <Skills/>
        <Projects/>
+       <GitHubStat/>
        <Contact/>
       </div>
     );
