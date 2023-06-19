822278e6b44016a8e3fc0b3a94328acc2bab038a
shabaz ali malik <shabazalimalik69@gmail.com>
2023-06-19 16:04:08 +0530

    Experience Updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,13 +13,25 @@ const Skills = () => {
             <h2 className='py-2 font-bold text-amber-500  text-1xl sm:text-2xl'>Microchip Payments	| Hubballi, Karnataka.</h2>
             <h2 className='py-2 font-bold text-amber-500  text-1xl sm:text-2xl'>March 2021 – June 2023</h2>
           <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
-          I worked at Microchip Payments, where I had the opportunity to work on several projects involving WhatsApp chatbot integration and backend API development. During my time at Microchip Payments, I gained valuable experience in the entire software development life cycle, from requirement gathering to testing and deployment.
+          As a Software Engineer at Microchip Payments, I specialized in working with React.js, Node.js, and WhatsApp ChatBot integration. Throughout my tenure, I played a critical role in developing and maintaining software applications related to payment processing, while also integrating chatbot capabilities into the WhatsApp platform.
  </h6>
  <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
- My primary focus was on integrating a WhatsApp chatbot into Microchip Payments existing system, allowing users to access essential HR services and interact with the company's HR platform seamlessly. I collaborated closely with a cross-functional team of developers, UI/UX designers, and product managers to ensure a smooth integration process and an exceptional user experience. Through this project, I honed my skills in API integration, webhook configuration, and building conversational user interfaces.
+ My responsibilities encompassed various aspects of software development, including frontend and backend development, chatbot integration and collaboration with cross-functional teams.
  </h6>
  <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
- In addition to the chatbot integration, I also worked on developing backend APIs to support various features of Microchip Payments. I actively contributed to the design and implementation of RESTful APIs, database schema design, and data modeling. My experience in this area has given me a solid understanding of scalable architecture and best practices for building robust and secure web applications.
+ In terms of software development, I actively participated in the creation of web-based applications using React.js for the frontend and Node.js for the backend. This involved closely working with the development team to understand business requirements, design software solutions, and implement them using best coding practices. Leveraging my expertise in React.js, I focused on designing and implementing user-friendly interfaces for payment-related modules. By creating responsive UI components, integrating APIs, and ensuring a seamless user experience across devices and browsers, I contributed to delivering visually appealing and intuitive interfaces.
+ </h6>
+ <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
+ Regarding backend development, I utilized Node.js to contribute to the creation of robust and scalable backend systems. I played a significant role in building RESTful APIs, implementing business logic, integrating databases, and ensuring the security and integrity of data. Additionally, I leveraged my knowledge of Node.js to optimize backend performance and effectively handle high-traffic scenarios, further enhancing the overall system performance.
+ </h6>
+ <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
+ One of my key contributions was the integration of chatbot capabilities into the WhatsApp platform. I was responsible for designing and developing conversational flows and integrating the chatbot with backend systems. By ensuring that the chatbot effectively handled user inquiries, provided relevant information, and seamlessly integrated with payment processing functionalities, I contributed to enhancing customer engagement and support capabilities.
+ </h6>
+ <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
+ During my tenure, I achieved significant milestones, including successfully integrating the WhatsApp ChatBot into the payment processing platform. This integration enhanced customer engagement and support capabilities, resulting in improved customer satisfaction and retention. I also delivered multiple features and enhancements, such as real-time transaction updates, payment status notifications, and personalized user experiences, further contributing to the overall success of the organization. I received positive feedback from clients and stakeholders for the quality of the developed software, its performance, and the seamless integration of chatbot functionality.
+ </h6>
+ <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
+ Overall, as a Software Engineer at Microchip Payments, I leveraged my expertise in React.js, Node.js, and WhatsApp ChatBot integration to develop and maintain software applications for payment processing. Through my contributions in software development, code review, testing, troubleshooting, and collaboration with cross-functional teams, I actively contributed to the growth and success.
  </h6>
           </div>
       </div>
