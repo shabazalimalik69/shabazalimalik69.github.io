f479c4940160812f1a4275c0e89cd6f9659ab13f
unknown <shabazalimalik69@gmail.com>
2024-09-03 20:58:25 +0530

    Experience updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,27 +1,69 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
-    <div name="experience" className='w-full h-full py-5  bg-[#293c42]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div>
-              <p className='text-4xl py-8 text-white font-bold text-center'>Experience</p>
-          </div>
-
-          <div className='w-full  text-center py-8'>
-          {/* <h1 className='py-3 text-2xl sm:text-5xl font-bold text-white'>Software Engineer</h1>
-            <h2 className='py-2 font-bold text-amber-500  text-1xl sm:text-2xl'>Microchip Payments	| Hubballi, Karnataka.</h2>
-            <h2 className='py-2 font-bold text-amber-500  text-1xl sm:text-2xl'>March 2021 – June 2023</h2> */}
-            <h2 className='py-3 text-2xl sm:text-5xl font-bold text-white'>Software Engineer Intern</h2>
-            <h2 className='py-2 font-bold text-amber-500  text-1xl sm:text-2xl'>Zimyo	| Gurgoan, Haryana.</h2>
-            <h2 className='py-2 font-bold text-amber-500  text-1xl sm:text-2xl'>March 2023 – May 2023</h2>
+    <div name="experience" className="w-full h-full py-5  bg-[#293c42]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl py-8 text-white font-bold text-center">
+            Experience
+          </p>
+        </div>
+{/* Spiderweb Technologies */}
+        <div className="w-full  text-center py-8">
+          <h2 className="py-3 text-2xl sm:text-5xl font-bold text-white">
+            Full Stack Developer
+          </h2>
+          <h2 className="py-2 font-bold text-amber-500  text-1xl sm:text-2xl">
+            Spiderweb Technologies | Mohali, Punjab.
+          </h2>
+          <h2 className="py-2 font-bold text-amber-500  text-1xl sm:text-2xl">
+            July 2023 – Present
+          </h2>
+          <h6 className="text-justify text-green-500 py-4 max-w-[900px]">
+            In my role, I have been deeply involved in the full lifecycle of
+            software development, from designing user-friendly interfaces to
+            implementing robust backend solutions.
+          </h6>
+          <h6 className="text-justify text-green-500 py-4 max-w-[900px]">
+            In terms of backend development, I specialized in building complex
+            RESTful APIs using Node.js and MongoDB. I integrated real-time chat
+            functionality with Firebase Realtime Database and Socket.io,
+            ensuring seamless communication within the application.
+          </h6>
+          <h6 className="text-justify text-green-500 py-4 max-w-[900px]">
+            Additionally, I implemented push notifications and email triggers
+            using NodeMailer, enhancing user engagement and system
+            notifications. My work also included optimizing backend performance
+            to handle high-traffic scenarios and ensuring data security and
+            integrity.
+          </h6>
+        </div>
+        {/* Microchip Payments */}
+        <div className="w-full  text-center py-8">
+          <h2 className="py-3 text-2xl sm:text-5xl font-bold text-white">
+            Software Engineer
+          </h2>
+          <h2 className="py-2 font-bold text-amber-500  text-1xl sm:text-2xl">
+            Microchip Payments | Hubballi, Karnataka.
+          </h2>
+          <h2 className="py-2 font-bold text-amber-500  text-1xl sm:text-2xl">
+            March 2021 – June 2023
+          </h2>
+          <h6 className="text-justify text-green-500 py-4  max-w-[900px] ">
+            As a Software Engineer at Microchip Payments, I specialized in
+            working with React.js, Node.js, and WhatsApp ChatBot integration.
+            Throughout my tenure, I played a critical role in developing and
+            maintaining software applications related to payment processing,
+            while also integrating chatbot capabilities into the WhatsApp
+            platform.
+          </h6>
+          <h6 className="text-justify text-green-500 py-4  max-w-[900px] ">
+            My responsibilities encompassed various aspects of software
+            development, including frontend and backend development, chatbot
+            integration and collaboration with cross-functional teams.
+          </h6>
           {/* <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
-          As a Software Engineer at Microchip Payments, I specialized in working with React.js, Node.js, and WhatsApp ChatBot integration. Throughout my tenure, I played a critical role in developing and maintaining software applications related to payment processing, while also integrating chatbot capabilities into the WhatsApp platform.
- </h6>
- <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
- My responsibilities encompassed various aspects of software development, including frontend and backend development, chatbot integration and collaboration with cross-functional teams.
- </h6>
- <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
  In terms of software development, I actively participated in the creation of web-based applications using React.js for the frontend and Node.js for the backend. This involved closely working with the development team to understand business requirements, design software solutions, and implement them using best coding practices. Leveraging my expertise in React.js, I focused on designing and implementing user-friendly interfaces for payment-related modules. By creating responsive UI components, integrating APIs, and ensuring a seamless user experience across devices and browsers, I contributed to delivering visually appealing and intuitive interfaces.
  </h6>
  <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
@@ -36,13 +78,10 @@ const Skills = () => {
  <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>
  Overall, as a Software Engineer at Microchip Payments, I leveraged my expertise in React.js, Node.js, and WhatsApp ChatBot integration to develop and maintain software applications for payment processing. Through my contributions in software development, code review, testing, troubleshooting, and collaboration with cross-functional teams, I actively contributed to the growth and success.
  </h6> */}
- <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>I recently completed a rewarding three-month internship at Zimyo, where I had the opportunity to work on several projects involving WhatsApp chatbot integration and backend API development. During my time at Zimyo, I gained valuable experience in the entire software development life cycle, from requirement gathering to testing and deployment.</h6>
- <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>My primary focus was on integrating a WhatsApp chatbot into Zimyo's existing system, allowing users to access essential HR services and interact with the company's HR platform seamlessly. I collaborated closely with a cross-functional team of developers, UI/UX designers, and product managers to ensure a smooth integration process and an exceptional user experience. Through this project, I honed my skills in API integration, webhook configuration, and building conversational user interfaces.</h6>
- <h6 className='text-justify text-green-500 py-4  max-w-[900px] '>In addition to the chatbot integration, I also worked on developing backend APIs to support various features of Zimyo's platform. I actively contributed to the design and implementation of RESTful APIs, database schema design, and data modeling. My experience in this area has given me a solid understanding of scalable architecture and best practices for building robust and secure web applications</h6>
-          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Skills;
